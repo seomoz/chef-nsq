de267f639e08eae535d5b656f8cf3465f70f3a0a
Phil H <phil.hildebrand@moz.com>
2020-03-03 12:51:49 -0800

    require semantic

--- recipes/nsqd.rb
+++ recipes/nsqd.rb
@@ -11,8 +11,7 @@
 include_recipe 'nsq'
 
 # chef_gem 'semantic'
-# require 'semantic'
-include 'semantic'
+require 'semantic'
 
 nsq_release = "nsq-#{node['nsq']['version']}-#{node['nsq']['go_version']}"
 
